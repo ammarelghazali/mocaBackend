5e26aca8f57fdb636885adfff59d5702a7087c01
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-27 16:26:53 +0200

    added district entity

---
+++ MOCA.Core/Entities/LocationManagment/District.cs
@@ -0,0 +1,18 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class District : BaseEntity
+    {
+        [ForeignKey("DistrictId")]
+        public long CityId { get; set; }
+        public virtual City City { get; set; }
+        public string DistrictName { get; set; }
+    }
+}
