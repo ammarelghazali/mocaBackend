0b5ecb1a6032d893d362d209929ea75dca5a338a
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-27 16:27:13 +0200

    modified district entity

--- MOCA.Core/Entities/LocationManagment/District.cs
+++ MOCA.Core/Entities/LocationManagment/District.cs
@@ -10,7 +10,7 @@ namespace MOCA.Core.Entities.LocationManagment
 {
     public class District : BaseEntity
     {
-        [ForeignKey("DistrictId")]
+        [ForeignKey("CityId")]
         public long CityId { get; set; }
         public virtual City City { get; set; }
         public string DistrictName { get; set; }
