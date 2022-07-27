54066b08dd4584062796800881e3af414fad2867
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-27 18:17:29 +0200

    added location type entity

---
+++ MOCA.Core/Enums/LocationManagment/LocationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Enums.LocationManagment
+{
+    public enum LocationType
+    {
+        Site = 1,
+        Spot = 2,
+        moca = 3,
+    }
+}
