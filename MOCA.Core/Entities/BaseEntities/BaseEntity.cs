9ef4ef0da76a0ee77253a9e4ad41d68d41222eb5
Mahmoud Beshbeshy <mahmoud_beshbeshy1996@hotmail.com>
2022-07-27 17:34:34 +0200

    SSO Entities

--- MOCA.Core/Entities/BaseEntities/BaseEntity.cs
+++ MOCA.Core/Entities/BaseEntities/BaseEntity.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MOCA.Core.Entities.BaseEntities
 {
     public abstract class BaseEntity
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public virtual long Id { get; set; }
         [Required]
         public string CreatedBy { get; set; }
---
+++ MOCA.Core/Entities/SSO/MemberShip_Categories.cs
@@ -0,0 +1,14 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+
+namespace MOCA.Core.Entities.SSO
+{
+    public class MemberShip_Categories : BaseEntity
+    {
+        public string Name { get; set; }
+        public long MainCategoryId { get; set; }
+        public MemberShip_Main_Categories MainCategory { get; set; }
+        public long BenefitTypeId { get; set; }
+        public MemberShip_Benefits_Types BenefitType { get; set; }
+        public bool IsStatic { get; set; }
+    }
+}
