8a455189d54c79cbd9408ba08f4bb85063efe447
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-27 18:16:26 +0200

    added location contact entity

---
+++ MOCA.Core/Entities/LocationManagment/LocationContact.cs
@@ -0,0 +1,23 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class LocationContact : BaseEntity
+    {
+        [ForeignKey("LocationID")]
+        public long LocationID { get; set; }
+        public virtual Location Location { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+    }
+}
