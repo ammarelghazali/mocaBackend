4c98c16a20224dd92e72ba00a5c7ce3a001e4f12
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-27 18:17:15 +0200

    added location currency entity

---
+++ MOCA.Core/Entities/LocationManagment/LocationCurrency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class LocationCurrency
+    {
+        [ForeignKey("LocationID")]
+        public long LocationID { get; set; }
+        public virtual Location Location { get; set; }
+        [ForeignKey("LocationCurrencyID")]
+        public long LocationCurrencyID { get; set; }
+        public virtual Currency Currency { get; set; }
+    }
+}
