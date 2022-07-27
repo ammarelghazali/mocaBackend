37874314bba75e9bf87329fed617093442be7606
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-27 16:05:37 +0200

    added Loc_Payment_Terms_ID as enum for location table

---
+++ MOCA.Core/Enums/LocationManagment/LocationPaymentMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace MOCA.Core.Enums.LocationManagment
+{
+    public enum LocationPaymentMethods
+    {
+        Cheques = 1,
+        DepositCheques = 2,
+        BankTransfer = 3,
+        Escrow = 4,
+        DigitalPayment = 5,
+    }
+}
