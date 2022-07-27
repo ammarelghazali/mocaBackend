68e63f399af85d7be65550e2655151cb8f1b9059
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-27 16:25:33 +0200

    added City Entity

---
+++ MOCA.Core/Entities/LocationManagment/City.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class City
+    {
+        [ForeignKey("CountryId")]
+        public long CountryId { get; set; }
+        public virtual Country Country { get; set; }
+        public string CityName { get; set; }
+    }
+}
