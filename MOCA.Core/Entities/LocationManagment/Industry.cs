9200053362368f3b7dfda5f033fa37a935bf463e
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-27 18:15:49 +0200

    added industry entity

---
+++ MOCA.Core/Entities/LocationManagment/Industry.cs
@@ -0,0 +1,16 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class Industry : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
