9136e44ec8ee4181bf663f672dfa1456fcbb5d35
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-27 18:15:37 +0200

    added currency entity

---
+++ MOCA.Core/Entities/LocationManagment/Currency.cs
@@ -0,0 +1,15 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class Currency : BaseEntity
+    {
+        public string Name { get; set; }
+        public string FlagCode { get; set; }
+    }
+}
