7c5e2ebb497fe3d746d618fd498d2dea8e5e95b1
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-28 10:22:41 +0200

    added location files entity

---
+++ MOCA.Core/Entities/LocationManagment/LocationFile.cs
@@ -0,0 +1,18 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class LocationFile : BaseEntity
+    {
+        [ForeignKey("LocationID")]
+        public long LocationID { get; set; }
+        public virtual Location Location { get; set; }
+        public string LocationContractFilePath { get; set; }
+    }
+}
