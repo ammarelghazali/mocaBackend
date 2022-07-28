03efd7eedd91463fd94bccb0467ff6f7fe20491a
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-28 10:36:51 +0200

    added Location Working Hour

---
+++ MOCA.Core/Entities/LocationManagment/LocationWorkingHour.cs
@@ -0,0 +1,21 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class LocationWorkingHour : BaseEntity
+    {
+        public TimeSpan StartWorkingHour { get; set; }
+        public TimeSpan EndWorkingHour { get; set; }
+        public string DayFrom { get; set; }
+        public string DayTo { get; set; }
+        [ForeignKey("LocationID")]
+        public long LocationID { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
