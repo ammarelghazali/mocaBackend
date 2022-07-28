41f28d187ce2cf3f41e0e87920126a4c99d73378
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-28 18:30:57 +0200

    update floor units in AppDbContext

--- MOCA.Presistence/Contexts/ApplicationDbContext.cs
+++ MOCA.Presistence/Contexts/ApplicationDbContext.cs
@@ -57,6 +57,7 @@ namespace MOCA.Presistence.Contexts
         public DbSet<Currency> Currencies { get; set; }
         public DbSet<District> Districts { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<FloorUnit> FloorUnits { get; set; }
         public DbSet<Inclusion> Inclusions { get; set; }
         public DbSet<Industry> Industries { get; set; }
         public DbSet<Location> Locations { get; set; }
