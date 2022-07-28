12db87bdd1ae5075c50ee8237a83619e9672ccee
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-28 17:24:40 +0200

    added Floor Building Entity

---
+++ MOCA.Core/Entities/LocationManagment/FloorBuilding.cs
@@ -0,0 +1,22 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class FloorBuilding : BaseEntity
+    {
+        public string Number { get; set; }
+        [ForeignKey("BuildingID")]
+        public long BuildingID { get; set; }
+        public virtual Building Building { get; set; }
+        public decimal GrossArea { get; set; }
+        public decimal NetArea { get; set; }
+        public int MaleRestroomCount { get; set; }
+        public int FemaleRestroomCount { get; set; }
+    }
+}
