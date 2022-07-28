bf9f3ee075eab29fc350ac9434bef8e7df0d4df8
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-28 10:42:52 +0200

    added Location Bank Account

---
+++ MOCA.Core/Entities/LocationManagment/LocationBankAccount.cs
@@ -0,0 +1,20 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class LocationBankAccount : BaseEntity
+    {
+        [ForeignKey("LocationID")]
+        public long LocationID { get; set; }
+        public virtual Location Location { get; set; }
+        public string BankName { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string SwiftCode { get; set; }
+    }
+}
