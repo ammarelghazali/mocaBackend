ef2fcc7e6be07488a230a02e739f8785d705c349
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-28 10:50:49 +0200

    added Service Fee Payments Due Date entity

---
+++ MOCA.Core/Entities/LocationManagment/ServiceFeePaymentsDueDate.cs
@@ -0,0 +1,19 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class ServiceFeePaymentsDueDate : BaseEntity
+    {
+        public DateTime DueDate { get; set; }
+        public decimal Amount { get; set; }
+        [ForeignKey("LocationID")]
+        public long LocationID { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
