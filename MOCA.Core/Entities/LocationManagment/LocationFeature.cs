2828461163c11c36eaf245df243d35876f25a485
Islam Zakaria <eslamzakaria293@gmail.com>
2022-07-28 16:10:55 +0200

    added location entities

---
+++ MOCA.Core/Entities/LocationManagment/LocationFeature.cs
@@ -0,0 +1,20 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class LocationFeature : BaseEntity
+    {
+        [ForeignKey("LocationID")]
+        public long LocationID { get; set; }
+        public virtual Location Location { get; set; }
+        [ForeignKey("FeatureID")]
+        public long FeatureID { get; set; }
+        public virtual Feature Feature { get; set; }
+    }
+}
