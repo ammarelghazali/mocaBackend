b6b1f1627fa236253cbaecec64fbdeb309b8c599
shaaban500 <moshaaban500@gmail.com>
2022-07-30 21:46:42 +0200

    Adds Events Repos in UnitOfWork

--- MOCA.Core/IUnitOfWork.cs
+++ MOCA.Core/IUnitOfWork.cs
@@ -1,10 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MOCA.Core.Entities.EventSpaceBookings;
+using MOCA.Core.Interfaces.Base;
+using MOCA.Core.Interfaces.Events;
+using MOCA.Core.Interfaces.Events.Repositories;
 
 namespace MOCA.Core
 {
     public interface IUnitOfWork : IDisposable
     {
         #region Moca Settings
+
+        #endregion
+
+        #region Events
+        IGenericRepository<EventRequester> EventRequesterRepo { get; }
+        IGenericRepository<EventCategory> EventCategoryRepo { get; }
+        IGenericRepository<EventReccurance> EventReccuranceRepo { get; }
+        IGenericRepository<EventAttendance> EventAttendanceRepo { get; }
+        IGenericRepository<EventType> EventTypeRepo { get; }
+        IContactDetailsRepository ContactDetailsRepo { get; }
+        IEventSpaceBookingRepository EventSpaceBookingRepo { get; }
+        IEventSpaceTimesRepository EventSpaceTimesRepo { get; }
+        IEventSpaceVenueRepository EventSpaceVenuesRepo { get; }
+        ISendEmailRepository SendEmailRepo { get; }
+        IInitiatedRepository InitiatedRepo { get; }
+        IOpportunityStageReportRepository OpportunityStageReportRepo { get; }
+        IOpportunityStageRepository OpportunityStageRepo { get; }
+        IEventOpportunityStatusRepository EventOpportunityStatusRepo { get; }
+        ILocationRepositoty LocationRepo { get; }
+        IIndustryRepository IndustryRepo { get; }
+        IAccountService AccountService { get; }
+        IUserService UserService { get; }
+        ILoungeClientRepository LoungeClientRepo { get; }
+        ILocationsMemberShipsRepository LocationsMemberShipsRepo { get; }
+        IEmailTemplateRepository EmailTemplateRepository { get; }
 
         #endregion
 
