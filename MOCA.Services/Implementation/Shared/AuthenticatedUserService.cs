77a544c6f35723d9b00eef00c94d68264edcf2d3
Sadek80 <mohamedsadk80@gmail.com>
2022-08-01 17:01:23 +0200

    Resolve Authentication Bug

--- MOCA.Services/Implementation/Shared/AuthenticatedUserService.cs
+++ MOCA.Services/Implementation/Shared/AuthenticatedUserService.cs
@@ -12,14 +12,14 @@ namespace MOCA.Services.Implementation.Shared
     {
         public AuthenticatedUserService(IHttpContextAccessor httpContextAccessor)
         {
-            //var User_Id = httpContextAccessor.HttpContext?.User?.FindFirst("uid").Value.ToString();
-            //if (!string.IsNullOrEmpty(User_Id)) { this.UserId = User_Id.ToString(); }
+            var User_Id = httpContextAccessor.HttpContext?.User?.FindFirst("uid").Value.ToString();
+            if (!string.IsNullOrEmpty(User_Id)) { this.UserId = User_Id.ToString(); }
 
-            //var User_Name = httpContextAccessor.HttpContext?.User?.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-            //if (!string.IsNullOrEmpty(User_Name.ToString())) { this.UserName = User_Name.ToString(); }
+            var User_Name = httpContextAccessor.HttpContext?.User?.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+            if (!string.IsNullOrEmpty(User_Name.ToString())) { this.UserName = User_Name.ToString(); }
 
-            //var EMail = httpContextAccessor.HttpContext?.User?.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
-            //if (!string.IsNullOrEmpty(EMail.ToString())) { this.Email = EMail.ToString(); }
+            var EMail = httpContextAccessor.HttpContext?.User?.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
+            if (!string.IsNullOrEmpty(EMail.ToString())) { this.Email = EMail.ToString(); }
         }
 
         public string UserName { get; }
