3f3d566fbe3861f9e052a21e1b5ada5034020826
Islam Zakaria <eslamzakaria293@gmail.com>
2022-08-01 19:57:10 +0200

    added Favourite location entity and table

---
+++ MOCA.Core/Entities/LocationManagment/FavouriteLocation.cs
@@ -0,0 +1,13 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using MOCA.Core.Entities.SSO;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class FavouriteLocation : BaseEntity
+    {
+        public long LocationId { get; set; }
+        public virtual Location Location { get; set; }
+        public long BasicUserId { get; set; }
+        public virtual BasicUser BasicUser { get; set; }
+    }
+}
