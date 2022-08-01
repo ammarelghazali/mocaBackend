d47799b411f9e3b2a70e9074ff20d2436df3b465
Sadek80 <mohamedsadk80@gmail.com>
2022-08-01 19:46:21 +0200

    Adds Base Entities for all Reservations and WorkSpaceReservations

---
+++ MOCA.Core/Entities/Shared/Reservations/ReservationTransaction.cs
@@ -0,0 +1,27 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MOCA.Core.Entities.Shared.Reservations
+{
+    public class ReservationTransaction : BaseEntity
+    {
+        [Required]
+        public long ReservationTypeId { get; set; }
+
+        [ForeignKey("ReservationTypeId")]
+        public ReservationType ReservationType { get; set; }
+
+        // TODO: Column to determinte the plan days for workspace
+
+        [Required]
+        public long ReservationTargetId { get; set; }
+
+        [Required]
+        public long RemainingHours { get; set; }
+
+        public int? TotalHours { get; set; }
+
+        public DateTime? ExtendExpiryDate { get; set; }
+    }
+}
