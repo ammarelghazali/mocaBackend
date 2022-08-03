e20f4f59d3a586e3ea5c9bf0d4438c349f3aeeb5
Sadek80 <mohamedsadk80@gmail.com>
2022-08-03 13:27:27 +0200

    Adds BaseReservationEntity

---
+++ MOCA.Core/Entities/Shared/Reservations/BaseReservationEntity.cs
@@ -0,0 +1,36 @@
+﻿using MOCA.Core.Entities.BaseEntities;
+using MOCA.Core.Entities.LocationManagment;
+using MOCA.Core.Entities.SSO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.Entities.Shared.Reservations
+{
+    public class BaseReservationEntity :  BaseEntity
+    {
+        [Required]
+        public long LocationId { get; set; }
+
+        [ForeignKey("LocationId")]
+        public Location Location { get; set; }
+
+        [Required]
+        public long BasicUserId { get; set; }
+
+        [ForeignKey("BasicUserId")]
+        public BasicUser BasicUser { get; set; }
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+
+        public long? PaymentMethodId { get; set; }
+
+        [ForeignKey("PaymentMethodId")]
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
