e9c0e509dff41859c8a50669b962a857737a9e66
Mahmoud Beshbeshy <mahmoud_beshbeshy1996@hotmail.com>
2022-08-04 20:06:43 +0200

    controllers for admin and user logins

---
+++ MOCA.Core/DTOs/SSO/Admin/GetClaimsRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOCA.Core.DTOs.SSO.Admin
+{
+    public class GetClaimsRequest
+    {
+        [Required]
+        public string Id { get; set; }
+    }
+}
--- SSO.API/Program.cs
+++ SSO.API/Program.cs
@@ -1,3 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MOCA.Core;
+using MOCA.Core.Interfaces.SSO.Services;
+using MOCA.Presistence;
+using MOCA.Presistence.Contexts;
+using MOCA.Services.Implementation.SSO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
@@ -7,6 +19,39 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
     options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
 });
 
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+                    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+builder.Services.AddAuthentication()
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = builder.Configuration["JWTSettings:Issuer"],
+                        ValidAudience = builder.Configuration["JWTSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:Key"]))
+                    };
+                });
+
+
+
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+#region Services
+builder.Services.AddScoped<IAdminService, AdminService>();
+builder.Services.AddScoped<IUserService, UserService>();
+#endregion
 // Add services to the container.
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
