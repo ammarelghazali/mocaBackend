614c4b989af1812e066b8be5f55b2d5ac57d1fc2
shaaban500 <moshaaban500@gmail.com>
2022-08-04 18:03:32 +0200

    Adds Meeting Reservations Services

--- MOCA.Core/Interfaces/MeetingSpaceReservations/Services/IMeetingReservationTopUpsServices.cs
+++ MOCA.Core/Interfaces/MeetingSpaceReservations/Services/IMeetingReservationTopUpsServices.cs
@@ -1,6 +1,6 @@
 ﻿namespace MOCA.Core.Interfaces.MeetingSpaceReservations.Services
 {
-    public interface IMeetingSpaceReservationsServices
+    public interface IMeetingReservationTopUpsServices
     {
     }
 }
