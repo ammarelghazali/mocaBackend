802870e38c4761690464800cb7016b31a5261a8a
shaaban500 <moshaaban500@gmail.com>
2022-08-08 22:25:02 +0200

    Adds Meeting Transactions & Cancellations Tables and Integrate with MeetingSpaces and Prices tables

--- MOCA.Presistence/Migrations/20220808201957_adds meeting reservation and cancellation tables and integrates with meetingSpaces tables.Designer.cs
+++ MOCA.Presistence/Migrations/20220808201957_adds meeting reservation and cancellation tables and integrates with meetingSpaces tables.Designer.cs
@@ -4,6 +4,7 @@ using MOCA.Presistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MOCA.Presistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220808201957_adds meeting reservation and cancellation tables and integrates with meetingSpaces tables")]
+    partial class addsmeetingreservationandcancellationtablesandintegrateswithmeetingSpacestables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
