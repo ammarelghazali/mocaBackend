08e670b70ca971b6b17b4a821ca9dcd6649be11b
shaaban500 <moshaaban500@gmail.com>
2022-08-09 03:18:59 +0200

    Adds Meeting Reservations CRM Controller

--- MeetingREservation.API/Controllers/MeetingSpaceReservationsController.cs
+++ MeetingREservation.API/Controllers/MeetingSpaceReservationsController.cs
@@ -77,8 +77,49 @@ namespace MeetingREservation.API.Controllers
         
         [HttpGet("GetAllLocations")]
         public async Task<IActionResult> GetAllLocations()
+    //[Authorize]
+    [ApiController]
+    public class MeetingSpaceReservationsController : Controller
+    {   
+        private readonly IMeetingSpaceReservationsServices _meetingSpaceReservationsServices;
+        public MeetingSpaceReservationsController(IMeetingSpaceReservationsServices meetingSpaceReservationsServices)
+        {
+            _meetingSpaceReservationsServices = meetingSpaceReservationsServices;
+        }
+
+        [HttpGet("GetAllSubmissionsWithPagination")]
+        public async Task<IActionResult> GetAllSubmissionsWithPagination(GetAllMeetingsSubmissionsDto dto)
+        {
+            var response = await _meetingSpaceReservationsServices.GetAllSubmissionsWithPagination(dto.PageNumber, dto.PageSize);
+            return Ok(response);
+        }
+
+
+        [HttpGet("GetAllSubmissionsWithoutPagination")]
+        public async Task<IActionResult> GetAllSubmissionsWithoutPagination()
+        {
+            var response = await _meetingSpaceReservationsServices.GetAllSubmissionsWithoutPagination();
+            return Ok(response);
+        }
+
+        [HttpGet("GetAllLocations")]
+        public async Task<IActionResult> GetAllLocations()
         {
             var response = await _meetingSpaceReservationsServices.GetAllMeetingReservationLocations();
+            return Ok(response);
+        }
+
+        [HttpGet("GetById")]
+        public async Task<IActionResult> GetMeetingReservationById(long id)
+        {
+            var response = await _meetingSpaceReservationsServices.GetMeetingReservationById(id);
+           return Ok(response);
+        }
+
+        [HttpGet("GetAllWithFilter")]
+        public async Task<IActionResult> GetAllWithFilter(GetAllMeetingReservationsWithFilterRequestDto dto)
+        {
+            var response = await _meetingSpaceReservationsServices.GetAllMeetingReservationsWithFilter(dto);
             return Ok(response);
         }
 
