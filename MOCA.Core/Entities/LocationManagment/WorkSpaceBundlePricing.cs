4fe8860e3ab493d0e7464d4dfebf369346fee748
Sadek80 <mohamedsadk80@gmail.com>
2022-08-10 15:41:20 +0200

    Adds Workspace Pricing

---
+++ MOCA.Core/Entities/LocationManagment/WorkSpaceBundlePricing.cs
@@ -0,0 +1,13 @@
+﻿using MOCA.Core.Entities.LocationManagment.Base;
+
+namespace MOCA.Core.Entities.LocationManagment
+{
+    public class WorkSpaceBundlePricing : BaseWorkSpaceBundlePricing
+    {
+        [Required]
+        public long WorkSpaceId { get; set; }
+
+        [ForeignKey("WorkSpaceId")]
+        public WorkSpace WorkSpace { get; set; }
+    }
+}
