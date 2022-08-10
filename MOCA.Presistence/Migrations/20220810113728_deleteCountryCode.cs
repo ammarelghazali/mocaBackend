21b7bb6203ca9812e98b4cbe5429f6b16e1cefa4
andrewemadgabra <anderwemad33@gmail.com>
2022-08-10 13:42:06 +0200

    location validation

---
+++ MOCA.Presistence/Migrations/20220810113728_deleteCountryCode.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MOCA.Presistence.Migrations
+{
+    public partial class deleteCountryCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "LocationContact");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "LocationContact",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
