0f4c436822267aa6e80f5ccba5c98e8b7ae0c257
Mahmoud Beshbeshy <mahmoud_beshbeshy1996@hotmail.com>
2022-08-10 18:08:06 +0200

    check github Acc

--- LocationManagement.API/Controllers/DynamicListsController.cs
+++ LocationManagement.API/Controllers/DynamicListsController.cs
@@ -33,7 +33,6 @@ namespace LocationManagement.API.Controllers
         public async Task<IActionResult> AddWorkSpaceCategory([FromBody] WorkSpaceCategoryModel model)
         {
             var data = await _WorkSpaceCategoryService.AddWorkSpaceCategory(model);
-
             if (data.Succeeded == false)
             {
                 return BadRequest(data);
