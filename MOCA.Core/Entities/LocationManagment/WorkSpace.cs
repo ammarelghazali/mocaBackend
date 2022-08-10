4d7e6849aa92637d8fdf3343a88b16953d0a15d2
Islam Zakaria <eslamzakaria293@gmail.com>
2022-08-10 16:20:29 +0200

    need to continue workspace

--- MOCA.Core/Entities/LocationManagment/WorkSpace.cs
+++ MOCA.Core/Entities/LocationManagment/WorkSpace.cs
@@ -1,30 +1,10 @@
-﻿using MOCA.Core.Entities.DynamicLists;
-using MOCA.Core.Entities.LocationManagment.Base;
-using MOCA.Core.Entities.WorkSpaceReservations;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using MOCA.Core.Entities.WorkSpaceReservations;
 
 namespace MOCA.Core.Entities.LocationManagment
 {
-    public class WorkSpace : BaseSpaceEntity
+    public class WorkSpace : BaseWorkSpace
     {
-        public decimal? GrossArea { get; set; }
-
-        public decimal? NetArea { get; set; }
-
-        [Required]
-        public long WorkSpaceTypeId { get; set; }
-
-        [ForeignKey("WorkSpaceTypeId")]
-        public WorkSpaceType WorkSpaceType { get; set; }
-
-        [Required]
-        public int MaximumOccupancy { get; set; }
-            
-        [Required]
-        public bool IsFurnishing { get; set; }
-
-        public ICollection<WorkSpaceReservationHourly> WorkSpaceReservationHourlies{ get; set; }
+        public ICollection<WorkSpaceReservationHourly> WorkSpaceReservationHourlies { get; set; }
         public ICollection<WorkSpaceReservationTailored> WorkSpaceReservationTailoreds { get; set; }
         public ICollection<WorkSpaceReservationBundle> WorkSpaceReservationBundles { get; set; }
     }
